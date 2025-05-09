0cf740a85b88a76e91b4a81f1600b92e12cb3872
Victor Nahuelcheo <victor.mnr@gmail.com>
2025-05-08 21:59:29 -0400

    Titular cambiado

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,6 +1,6 @@
 
 export default function Header() {
     return (
-        <div className='bg-gray-50 pt-12'>MOTION GRAPHICS</div>
+        <div className='bg-gray-50 pt-12'>MOTION GRAPHICS De Bick 2025</div>
     )
 }
