fbc06d5089bde7e7e2920d500c5b13ba11b2ed53
Victor Nahuelcheo <victor.mnr@gmail.com>
2025-05-09 22:01:09 -0400

    Logo Bick colocado en header.tsx

--- src/Components/header.tsx
+++ src/Components/header.tsx
@@ -1,6 +1,18 @@
+import logoUrl from '../assets/pill.svg'; // Importación por defecto (como URL)
 
 export default function Header() {
-    return (
-      <div className='bg-gray-50 font-work-sans text-primary pt-12 font-bold leading-20 text-[5.625rem] text-left pl-8'>MOTION <br/> GRAPHICS </div>
-    )
+  return (
+    <>
+      <div className='bg-gray-50 static font-work-sans text-primary pt-12 font-extrabold leading-20 text-[5.625rem] text-left pl-8'>
+        MOTION <br/> GRAPHICS
+      </div>
+      <div className="relative">
+        <img
+          src={logoUrl}
+          alt="Logo"
+          className="h-12 w-auto relative bottom-2 left-80 -rotate-7 z-10"
+        />
+      </div>
+    </>
+  );
 }
